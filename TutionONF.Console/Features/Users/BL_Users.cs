49a607e9c8e43e0b12f81a54359fedfd54efd9a3
Pyae Phyo Kyaw <pyaephyokyaw.dev@gmail.com>
2024-11-13 16:11:31 +0630

    Testing commit

--- TutionONF.Console/Features/Users/BL_Users.cs
+++ TutionONF.Console/Features/Users/BL_Users.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TutionONF.Console.Features.Users;
 
-namespace TutionONF.Console.Features.Users
+public class BL_Users
 {
-    internal class BL_Users
-    {
-    }
 }
